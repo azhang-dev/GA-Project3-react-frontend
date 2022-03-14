c52348fa13a2c7381ed6793343272791c3b38703
Angela Zhang <a_zhang93@hotmail.com>
2022-03-14 23:43:42 +1100

    login/signup form setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './Header'
+import SignUpForm from './components/SignUpForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const [user, setUser] = useState({})
+  const [form, setForm] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if(token){
+      fetch(`http://localhost:3000/auto_login`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        setUser(data)
+        //console.log(data)
+      })
+    }
+  },[])
+
+  const handleLogin = (user) => {
+    setUser(user)
+  }
+
+  const handleFormSwitch = (input) => {
+    setForm(input)
+  }
+
+  const handleAuthClick = () => {
+    const token = localStorage.getItem("token")
+    fetch(`http://localhost:3000/user_is_authed`,{
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+  console.log(user)
+
+  const renderForm = () => {
+    switch(form){
+      case "login":
+        return <LoginForm handleLogin={handleLogin}/>
+        break;
+      default:
+        return <SignUpForm handleLogin={handleLogin}/>
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react'
+
+function SignUpForm(props) {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const API_BASE_URL =  "http://localhost:3000/"
+    const handleNameChange = (ev) => {
+        setName(ev.target.value)
+    }
+
+    const handleEmailChange = (ev) => {
+        setEmail(ev.target.value)
+    }
+    const handleUsernameChange = (ev) => {
+        setUsername(ev.target.value)
+    }
+
+    const handlePasswordChange = (ev) => {
+        setPassword(ev.target.value)
+    }
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        fetch(`${API_BASE_URL}users`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                username,
+                password
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            localStorage.setItem("token", data.jwt)
+            props.handleLogin(data.user)
+        })
+        setUsername("")
+        setPassword("")
+    }
+
+    const formDivStyle = {
+        margin: "auto",
+        padding: "20px",
+        width: "80%"
+    }
+
+    return(
+        <div style={formDivStyle}>
+            <h1> Sign Up</h1>
+            <form className="ui form" onSubmit={handleSubmit}>
+                <div className='field'>
+                    <label>Name</label>
+                    <input value={name} onChange={handleNameChange} type="text" placeholder="name"/>
+                </div>
+                <div className='field'>
+                    <label>email</label>
+                    <input value={email} onChange={handleEmailChange} type="text" placeholder="email"/>
+                </div>
+                <div className='field'>
+                    <label>Username</label>
+                    <input value={username} onChange={handleUsernameChange} type="text" placeholder="username"/>
+                </div>
+                <div className='field'>
+                    <label>Password</label>
+                    <input value={password} onChange={handlePasswordChange} type="text" placeholder="password"/>
+                </div>
+
+                <button className='ui button' type='submit'>Submit</button>
+
+            </form>
+
+        </div>
+    )
+
+}
+
+export default SignUpForm
