d695d58705c8a002c69b2d6fc11361c41bb311fa
Angela Zhang <a_zhang93@hotmail.com>
2022-03-16 18:58:29 +1100

    search bar working-fixed map key warning

--- src/components/Places/SinglePlaceMap.js
+++ src/components/Places/SinglePlaceMap.js
@@ -1,12 +1,11 @@
 import React from "react";
-import {GoogleMap, useLoadScript, Marker, InfoWindow, useJsApiLoader, LoadScript } from "@react-google-maps/api";
-import {GOOGLE_MAP_API_KEY} from "../../mapApiBaseUrl";
+import {GoogleMap, useLoadScript, Marker, InfoWindow} from "@react-google-maps/api";
+// import {GOOGLE_MAP_API_KEY} from "../../mapApiBaseUrl";
 // import { API_ROOT } from '../../constants';
 import axios from "axios";
-import {formatRelative} from "date-fns"
 // import 'dotenv/config'
 
-import usePlaceAutocomplete, {
+import usePlacesAutocomplete, {
     getGeocode,
     getLatLng,
 } from "use-places-autocomplete";
@@ -18,6 +17,7 @@ import {
     ComboboxList,
     ComboboxOption,
 } from "@reach/combobox";
+import { formatRelative } from "date-fns";
 import "@reach/combobox/styles.css";
 import "../../stylesheets/placeMap.css"
 
@@ -75,48 +75,50 @@ export default function SinglePlaceMap() {
     if(!isLoaded) return "Loading Maps";
 
 
-    return <div>
+    return (    
+        <div>
 
-        <Search />
+            <Search />
 
-        <GoogleMap 
-            mapContainerStyle={mapContainerStyle}
-            zoom ={10}
-            center={center}
-            options={options}
-            onClick = {onMapClick}
-            onLoad={onMapLoad}
-        >
-            {markers.map(marker => <Marker 
-                key={marker.time.toISOString()} 
-                position={{lat: marker.lat, lng:marker.lng }}
-                onClick={() => {
-                    setSelected(marker); // on click saves the selected marker to the selectedState
-                }}
-            />)}
+            <GoogleMap 
+                mapContainerStyle={mapContainerStyle}
+                zoom ={10}
+                center={center}
+                options={options}
+                onClick = {onMapClick}
+                onLoad={onMapLoad}
+            >
+                {markers.map(marker => <Marker 
+                    key={marker.time.toISOString()} 
+                    position={{lat: marker.lat, lng:marker.lng }}
+                    onClick={() => {
+                        setSelected(marker); // on click saves the selected marker to the selectedState
+                    }}
+                />)}
 
-            {selected ? (
-                <InfoWindow position={{lat: selected.lat, lng: selected.lng}} onCloseClick={() => {
-                    setSelected(null);// reset setSelected so inforWindow can be shown when selecting a new marker- toggling it on an off
-                }}>
-                    <div>
-                        <h2>"Example: bearspotted!"</h2>
-                        <p>Spotted: {formatRelative(selected.time, new Date())}</p>
-                    </div>
-                </InfoWindow>) : null}
-        
-        </GoogleMap>
-    </div>;
+                {selected ? (
+                    <InfoWindow position={{lat: selected.lat, lng: selected.lng}} onCloseClick={() => {
+                        setSelected(null);// reset setSelected so inforWindow can be shown when selecting a new marker- toggling it on an off
+                    }}>
+                        <div>
+                            <h2>"Example: Location Marked!"</h2>
+                            <p>Marked at: {formatRelative(selected.time, new Date())}</p>
+                        </div>
+                    </InfoWindow>) : null}
+            
+            </GoogleMap>
+        </div>
+    );
 }
 
-function Search(){
+function Search() {
     const {
         ready, 
         value, 
-        suggestions: {status,data}, 
+        suggestions: { status, data }, 
         setValue, 
         clearSuggestions,
-    } = usePlaceAutocomplete({
+    } = usePlacesAutocomplete({
         requestOptions: {
             location: {lat: () => -12.046373, lng: () => -77.042755},
             radius: 200 * 1000, // * 1000 because its in meters 
@@ -125,24 +127,33 @@ function Search(){
     
     return (
         <div className="search-box">
-            <Combobox 
-                onSelect={(address) => {
-                    console.log(address);
-                }}
-            >
-                <ComboboxInput value={value} onChange={(e) => {
-                    console.log(e.target.value)   
-                    setValue(e.target.value);
-                }}
-                disabled={!ready}
-                placeholder="Enter an address"
+            <Combobox onSelect={ async (address) => {
+                try {
+                    const results = await getGeocode({address});
+                    const {lat, lng} = await getLatLng(results[0]);
+                    // console.log(results[0]);
+                    console.log(`lat: ${lat},lng: ${lng}`);
+                } catch(err){
+                    console.log("error:",err)
+                }
+                console.log(address)
+                }}>
+                <ComboboxInput 
+                    value={value} 
+                    onChange={(e) => {setValue(e.target.value)}}
+                    disabled={!ready}
+                    placeholder="Enter an address"
                 />
                 <ComboboxPopover>
-                    {status === "OK" && data.map(({id, description}) => (
-                    <ComboboxOption key={id} value={description} />
-                    ))}
+                    <ComboboxList className="search-sugestion-text">
+                        {status === "OK" && data.map(({place_id, description}) => {
+                        
+                            return (<ComboboxOption key={place_id} value={description} />)
+                        }
+                        )}
+                    </ComboboxList>
                 </ComboboxPopover>
             </Combobox>
         </div>
-    )
+    );
 }
