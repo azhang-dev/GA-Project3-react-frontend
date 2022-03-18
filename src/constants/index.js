9070395e38c509ed45b5061222674f8a3d93cdd2
Angela Zhang <a_zhang93@hotmail.com>
2022-03-19 10:30:07 +1100

    fixed heroku API_ROOT url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = process.env.NODE_ENV === "production" ? "https://obscure-brushlands-14937.herokuapp.com/" : "http://localhost:3000"
+export const API_ROOT = process.env.NODE_ENV === "production" ? "https://obscure-brushlands-14937.herokuapp.com" : "http://localhost:3000"
 
 export const HEADERS = {
   'Content-Type': 'application/json',
